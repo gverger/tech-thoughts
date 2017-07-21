43ec0638b26b318ea7cb091de50e256f5624b4af
Guillaume Verger <verger.guillaume@gmail.com>
2017-07-21 12:18:21 -0700

    More Styling

--- config.rb
+++ config.rb
@@ -65,8 +65,8 @@ activate :syntax
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 end
